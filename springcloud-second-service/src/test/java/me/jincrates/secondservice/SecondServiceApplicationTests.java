9b53beb27871bcfac0d9742a6dd72a61223ddb39
jincrates <94jingyu@gmail.com>
2021-11-16 08:17:35 +0900

    add service

---
+++ springcloud-second-service/src/test/java/me/jincrates/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
