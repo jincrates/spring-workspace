a4541b45a3195e6957f652a642fbe5e64f32e2a3
jincrates <94jingyu@gmail.com>
2021-11-15 19:24:46 +0900

    Spring Cloud Gateway 설정

--- springcloud-gateway/src/main/java/me/jincrates/gateway/GatewayApplication.java
+++ springcloud-gateway/src/main/java/me/jincrates/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
-
+	// 서버가 tomcat이 아닌 비동기 방식의 netty가 실행됨
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
