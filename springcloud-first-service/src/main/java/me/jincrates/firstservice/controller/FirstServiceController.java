c17af9a4d3cb85f74f024e26ee5fdeb66d7f4c8e
jincrates <94jingyu@gmail.com>
2021-11-17 08:00:17 +0900

    Spring Cloud Gateway - Filter 적용

--- springcloud-first-service/src/main/java/me/jincrates/firstservice/controller/FirstServiceController.java
+++ springcloud-first-service/src/main/java/me/jincrates/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,8 @@
 package me.jincrates.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 //https://localhost:8081/first-service/welcome
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in Fisrt Service.";
     }
 }
