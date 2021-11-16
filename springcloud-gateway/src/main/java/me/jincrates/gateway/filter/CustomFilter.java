b5402f9a820645cad04913c93d2d6cf48b1a813f
jincrates <94jingyu@gmail.com>
2021-11-17 08:31:11 +0900

    Spring Cloud Gateway - Custome Filter 적용(Pre/Post 구분 가능)

---
+++ springcloud-gateway/src/main/java/me/jincrates/gateway/filter/CustomFilter.java
@@ -0,0 +1,40 @@
+package me.jincrates.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        //Custom Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custome PRE filter: request id -> {}", request.getId());
+
+            // Custom Post Filter
+            //Mono: Spring 5에서 추가됨(비동기방식 서버로 단일값 전달할 때 사용)
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+
+    public static class Config {
+        // Put the configuration properties
+    }
+}
