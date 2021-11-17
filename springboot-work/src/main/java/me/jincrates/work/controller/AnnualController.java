7ab51ba2d059d572ed5699cca79b43428d7f3fdf
jincrates <94jingyu@gmail.com>
2021-11-18 07:57:44 +0900

    add views

---
+++ springboot-work/src/main/java/me/jincrates/work/controller/AnnualController.java
@@ -0,0 +1,19 @@
+package me.jincrates.work.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/annual")
+public class AnnualController {
+
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+
+
+}
