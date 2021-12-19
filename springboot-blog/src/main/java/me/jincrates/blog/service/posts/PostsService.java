f4c1db2bfb8c90b888a158bf626e9c296efd7dd9
jincrates <94jingyu@gmail.com>
2021-12-19 10:20:21 +0900

    게시글 리스트 화면 구현

--- springboot-blog/src/main/java/me/jincrates/blog/service/posts/PostsService.java
+++ springboot-blog/src/main/java/me/jincrates/blog/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package me.jincrates.blog.service.posts;
 import lombok.RequiredArgsConstructor;
 import me.jincrates.blog.domain.posts.Posts;
 import me.jincrates.blog.domain.posts.PostsRepository;
+import me.jincrates.blog.web.dto.posts.PostsListResponseDto;
 import me.jincrates.blog.web.dto.posts.PostsResponseDto;
 import me.jincrates.blog.web.dto.posts.PostsSaveRequestDto;
 import me.jincrates.blog.web.dto.posts.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,9 +33,17 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
