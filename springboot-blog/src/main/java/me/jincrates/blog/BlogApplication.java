78a36a9c4dc602a161760e40acb4420cafddec4f
jincrates <94jingyu@gmail.com>
2021-12-20 17:49:24 +0900

    application-real 추가

--- springboot-blog/src/main/java/me/jincrates/blog/BlogApplication.java
+++ springboot-blog/src/main/java/me/jincrates/blog/BlogApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+//@EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
 public class BlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
 }
---
+++ springboot-blog/src/main/java/me/jincrates/blog/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package me.jincrates.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing  // JPA Auditing 활성화
+public class JpaConfig {
+}
