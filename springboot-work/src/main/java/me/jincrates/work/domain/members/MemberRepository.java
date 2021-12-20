d4098ed087bb27a2ba384cf0c0d7186ab7a51171
jincrates <94jingyu@gmail.com>
2021-12-21 08:32:56 +0900

    Member Entity 수정

---
+++ springboot-work/src/main/java/me/jincrates/work/domain/members/MemberRepository.java
@@ -0,0 +1,10 @@
+package me.jincrates.work.domain.members;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
