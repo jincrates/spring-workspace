d9be6bc7cb0f02bae8dfe2b7f03014cd41f1a74b
jincrates <94jingyu@gmail.com>
2021-12-25 21:14:18 +0900

    기초데이터 추가

---
+++ springboot-blog/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into posts (title, content, author, created_date, modified_date) values ('title1', 'content1', 'author1', now(), now())
+insert into posts (title, content, author, created_date, modified_date) values ('title2', 'content2', 'author2', now(), now())
+insert into posts (title, content, author, created_date, modified_date) values ('title3', 'content3', 'author3', now(), now())
