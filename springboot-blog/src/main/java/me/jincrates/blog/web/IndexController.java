bb2e8249d44085c5cd8138ee8b88d5153b6e4780
jincrates <94jingyu@gmail.com>
2021-12-30 08:35:55 +0900

    글 등록자 사용자 아이디 가져오도록 수정

--- springboot-blog/src/main/java/me/jincrates/blog/web/IndexController.java
+++ springboot-blog/src/main/java/me/jincrates/blog/web/IndexController.java
@@ -28,7 +28,10 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(Model model, @LoginUser SessionUser user) {
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "posts/posts-save";
     }
 
