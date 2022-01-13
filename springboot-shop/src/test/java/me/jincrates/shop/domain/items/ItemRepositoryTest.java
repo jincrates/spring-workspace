0b21c1775f61907cbc72951599d10a3891f66818
jincrates <94jingyu@gmail.com>
2022-01-14 07:47:28 +0900

    상품등록 리스트 테스트 코드작성

--- springboot-shop/src/test/java/me/jincrates/shop/domain/items/ItemRepositoryTest.java
+++ springboot-shop/src/test/java/me/jincrates/shop/domain/items/ItemRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,7 +18,7 @@ public class ItemRepositoryTest {
     ItemRepository itemRepository;
 
     @Test
-    public void 상품_저장_테스트() {
+    public void 상품저장_테스트() {
         Item item = Item.builder()
                 .itemNm("테스트 상품")
                 .price(10000)
@@ -25,5 +28,28 @@ public class ItemRepositoryTest {
                 .build();
         Item savedItem = itemRepository.save(item);
         System.out.print(savedItem);
+    }
+
+    @Test
+    public void 상품저장_리스트_테스트() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품 " + i)
+                    .price(10000)
+                    .itemDetail("테스트 상품 상세 설명 " + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .stockNumber(100)
+                    .build();
+            Item savedItem = itemRepository.save(item);
+        });
+    }
+
+    @Test
+    public void 상품명_조회_테스트() {
+        this.상품저장_리스트_테스트();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품 1");
+        for(Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
