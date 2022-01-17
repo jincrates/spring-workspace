4153d8c003b3d192ece19468ea24c19292f30c53
jincrates <94jingyu@gmail.com>
2022-01-17 21:40:26 +0900

    @Query 어노테이션 사용

--- springboot-shop/src/main/java/me/jincrates/shop/domain/items/ItemRepository.java
+++ springboot-shop/src/main/java/me/jincrates/shop/domain/items/ItemRepository.java
@@ -1,6 +1,8 @@
 package me.jincrates.shop.domain.items;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     //OrderBy로 정렬 처리하기
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
--- springboot-shop/src/test/java/me/jincrates/shop/domain/items/ItemRepositoryTest.java
+++ springboot-shop/src/test/java/me/jincrates/shop/domain/items/ItemRepositoryTest.java
@@ -48,34 +48,46 @@ public class ItemRepositoryTest {
     public void 상품명_조회() {
         this.상품저장_리스트_조회();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품 1");
-        for(Item item : itemList) {
-            System.out.println(item.toString());
-        }
+        printList(itemList);
     }
 
     @Test
     public void 상품명_OR_상품상세설명_조회() {
         this.상품저장_리스트_조회();
         List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품 1", "테스트 상품 상세 설명 5");
-        for(Item item : itemList) {
-            System.out.println(item.toString());
-        }
+        printList(itemList);
     }
 
     @Test
     public void 가격_LessThan_조회() {
         this.상품저장_리스트_조회();
         List<Item> itemList = itemRepository.findByPriceLessThan(10005);
-        for(Item item : itemList) {
-            System.out.println(item.toString());
-        }
+        printList(itemRepository.findByPriceLessThan(10005));
     }
 
     @Test
     public void 가격_내림차순_조회() {
         this.상품저장_리스트_조회();
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
-        for(Item item : itemList) {
+        printList(itemList);
+    }
+
+    @Test
+    public void  Query를_이용한_상품조회() {
+        this.상품저장_리스트_조회();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명 1");
+        printList(itemList);
+    }
+
+    @Test
+    public void  nativeQuery를_이용한_상품조회() {
+        this.상품저장_리스트_조회();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명 1");
+        printList(itemList);
+    }
+
+    public void printList(List<Item> itemList) {
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
