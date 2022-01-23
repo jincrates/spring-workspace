7fed601b754009349dcd7967522fd2f03b15f34f
jincrates <94jingyu@gmail.com>
2022-01-24 07:50:10 +0900

    회원가입 테스트 코드 작성

---
+++ springboot-shop/src/main/java/me/jincrates/shop/service/members/MemberService.java
@@ -0,0 +1,27 @@
+package me.jincrates.shop.service.members;
+
+import lombok.RequiredArgsConstructor;
+import me.jincrates.shop.domain.members.Member;
+import me.jincrates.shop.domain.members.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
---
+++ springboot-shop/src/test/java/me/jincrates/shop/service/members/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package me.jincrates.shop.service.members;
+
+import me.jincrates.shop.domain.members.Member;
+import me.jincrates.shop.web.dto.members.MemberFormDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember() {
+        Member entity = Member.builder()
+                .email("test@email.com")
+                .name("홍길동")
+                .address("서울시 강서구 화곡본동")
+                .password("1234")
+                .build();
+        MemberFormDto memberFormDto = new MemberFormDto(entity);
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    public void 회원가입() {
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAddress(), savedMember.getAddress());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getRole(), savedMember.getRole());
+    }
+}
