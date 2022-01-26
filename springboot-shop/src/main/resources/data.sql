1679cf03b3e81665f7a729d7167837b09616936e
jincrates <94jingyu@gmail.com>
2022-01-27 08:52:17 +0900

    페이지 접근 권한 설정

--- springboot-shop/src/main/resources/data.sql
+++ springboot-shop/src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO MEMBER (MEMBER_ID, NAME, EMAIL, PASSWORD, ADDRESS, ROLE) VALUES (1, 'admin', 'admin@email.com', '$2a$10$6xrKemS5FxTzY6da.QBsWuRm3jKv8pwWfpBRuptADDvLt/K.YMYVG', '서울시 강서구', 'USER');
+INSERT INTO MEMBER (MEMBER_ID, NAME, EMAIL, PASSWORD, ADDRESS, ROLE) VALUES (1, 'admin', 'admin@email.com', '$2a$10$6xrKemS5FxTzY6da.QBsWuRm3jKv8pwWfpBRuptADDvLt/K.YMYVG', '서울시 강서구', 'ADMIN');
+INSERT INTO MEMBER (MEMBER_ID, NAME, EMAIL, PASSWORD, ADDRESS, ROLE) VALUES (2, 'user', 'user@email.com', '$2a$10$6xrKemS5FxTzY6da.QBsWuRm3jKv8pwWfpBRuptADDvLt/K.YMYVG', '서울시 강서구', 'USER');
