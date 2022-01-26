3e3cb8b5a0d8d31b0d7a004f61faac51c5e4da0b
jincrates <94jingyu@gmail.com>
2022-01-26 10:32:05 +0900

    회원가입 기능 구현 중 dto 데이터가 null로 찍히네

--- springboot-shop/src/main/java/me/jincrates/shop/web/dto/members/MemberFormDto.java
+++ springboot-shop/src/main/java/me/jincrates/shop/web/dto/members/MemberFormDto.java
@@ -3,14 +3,31 @@ package me.jincrates.shop.web.dto.members;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import me.jincrates.shop.domain.members.Member;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@ToString
 @Getter
 @NoArgsConstructor
 public class MemberFormDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=4, max=16, message = "비밀번호는 4자 이상, 16자 이하로 입력해주세요.")
     private String password;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     @Builder
