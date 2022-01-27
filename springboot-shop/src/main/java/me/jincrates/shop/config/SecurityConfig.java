f68e2452dafd7a95e935e44dfd30c9bc5bcd1d6a
jincrates <94jingyu@gmail.com>
2022-01-28 08:36:34 +0900

    commit 충돌

--- springboot-shop/src/main/java/me/jincrates/shop/config/SecurityConfig.java
+++ springboot-shop/src/main/java/me/jincrates/shop/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
         ;
+
     }
 
     @Bean
