0b18c53bd91236e31077a78a22a502033d19d737
jincrates <94jingyu@gmail.com>
2022-02-16 08:36:06 +0900

    구매 이력 리스트 개발 중

--- springboot-shop/src/main/java/me/jincrates/shop/service/order/OrderService.java
+++ springboot-shop/src/main/java/me/jincrates/shop/service/order/OrderService.java
@@ -2,6 +2,8 @@ package me.jincrates.shop.service.order;
 
 import lombok.RequiredArgsConstructor;
 import me.jincrates.shop.domain.items.Item;
+import me.jincrates.shop.domain.items.ItemImg;
+import me.jincrates.shop.domain.items.ItemImgRepository;
 import me.jincrates.shop.domain.items.ItemRepository;
 import me.jincrates.shop.domain.members.Member;
 import me.jincrates.shop.domain.members.MemberRepository;
@@ -9,6 +11,11 @@ import me.jincrates.shop.domain.order.Order;
 import me.jincrates.shop.domain.order.OrderItem;
 import me.jincrates.shop.domain.order.OrderRepository;
 import me.jincrates.shop.web.dto.order.OrderDto;
+import me.jincrates.shop.web.dto.order.OrderHistDto;
+import me.jincrates.shop.web.dto.order.OrderItemDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +31,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId())
@@ -38,5 +46,28 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 }
---
+++ springboot-shop/src/main/java/me/jincrates/shop/web/dto/order/OrderItemDto.java
@@ -0,0 +1,24 @@
+package me.jincrates.shop.web.dto.order;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.jincrates.shop.domain.order.OrderItem;
+
+@Getter @Setter
+public class OrderItemDto {
+
+    private String itemNm;  //상품명
+
+    private int count;  //주문 수량
+
+    private int orderPrice; //주문 금액
+
+    private String imgUrl;  //상품 이미지 경로
+
+    public OrderItemDto(OrderItem orderItem, String imgUrl) {
+        this.itemNm = orderItem.getItem().getItemNm();
+        this.count = orderItem.getCount();
+        this.orderPrice = orderItem.getOrderPrice();
+        this.imgUrl = imgUrl;
+    }
+}
