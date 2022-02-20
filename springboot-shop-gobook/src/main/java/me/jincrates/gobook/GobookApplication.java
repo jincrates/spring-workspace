5caba485619c530fa90f519cfa691485ad6eb309
jincrates <94jingyu@gmail.com>
2022-02-20 16:52:26 +0900

    init

---
+++ springboot-shop-gobook/src/main/java/me/jincrates/gobook/GobookApplication.java
@@ -0,0 +1,13 @@
+package me.jincrates.gobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobookApplication.class, args);
+	}
+
+}
