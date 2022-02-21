3ec2f3d6e2e19c2ed45ada06a170d49e3b72aeab
jincrates <94jingyu@gmail.com>
2022-02-22 07:44:31 +0900

    1. 문자열 찾기

--- coding-test-java/src/main/java/me/jincrates/codingtest/a_String/Session01.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/a_String/Session01.java
@@ -1,4 +1,31 @@
 package me.jincrates.codingtest.a_String;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Session01 {
+
+    public static int solution(String str, char t) {
+        int answer = 0;
+
+        //대문자로 변환
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        for (int i = 0, max = str.length(); i < max; i++) {
+            if (str.charAt(i) == t) {
+               answer += 1;
+            }
+        }
+
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.println(solution(str, c));
+    }
 }
