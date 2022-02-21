d28766262d72b0924f71607cc1f6df7686c4c981
jincrates <94jingyu@gmail.com>
2022-02-22 08:11:13 +0900

    기본 포맷

---
+++ coding-test-java/src/main/java/me/jincrates/codingtest/a_String/Session00.java
@@ -0,0 +1,21 @@
+package me.jincrates.codingtest.a_String;
+
+
+import java.util.Scanner;
+
+public class Session00 {
+
+    public static int solution(String str, char t) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.println(solution(str, c));
+    }
+}
