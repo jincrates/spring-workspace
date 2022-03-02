03134f81305d3287181a39bd3f030985483ecdfa
jincrates <94jingyu@gmail.com>
2022-03-03 08:10:07 +0900

    4. 피보나치 수열(배열 안쓰고)

--- coding-test-java/src/main/java/me/jincrates/codingtest/b_Array/Session04.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/b_Array/Session04.java
@@ -3,26 +3,24 @@ package me.jincrates.codingtest.b_Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//4. 피보나치 수열
+//4. 피보나치 수열(배열 안쓰고)
 public class Session04 {
-    public static int[] solution(int num) {
-        int[] answer = new int[num];
-        answer[0] = 1;
-        answer[1] = 1;
-        for (int i = 2; i < num; i++) {
-            answer[i] = answer[i-2] + answer[i-1];
-        }
+    public static void solution(int num) {
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b);
 
-        return answer;
+        for (int i = 2; i < num; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
     }
 
     //10
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int total = kb.nextInt();
-
-        for (Integer result : solution(total)) {
-            System.out.print(result + " ");
-        }
+        solution(total);
     }
 }
