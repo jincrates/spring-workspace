6bc69635a29b0b2325e1b5c4a991acab5907f4d8
jincrates <94jingyu@gmail.com>
2022-03-10 08:03:46 +0900

    10. 봉우리(좌표 값으로)

--- coding-test-java/src/main/java/me/jincrates/codingtest/b_Array/Session10.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/b_Array/Session10.java
@@ -4,19 +4,38 @@ import java.util.Scanner;
 
 //10. 봉우리
 public class Session10 {
+
     public static int solution(int n, int[][] arr) {
         int answer = 0;
-        
-        for (int i = 1; i < n + 1; i++){
-            for(int j = 1; j < n + 1; j++) {
-                //System.out.println("arr[" + i + "][" + j + "] = " + arr[i][j]);
-                //System.out.print(arr[i][j] + " ");
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k]; // 행좌표
+                    int ny = j + dy[k]; // 열좌표
+
+                    //좌표에 따른 값이 기준점보다 큰게 있는지 검증
+                    if (nx >=0 && nx < n && ny >= 0 && ny < n) {
+                        if (arr[nx][ny] >= arr[i][j]) {
+                            flag = false;   // 봉우리가 아니다.
+                            break;
+                        }
+                    }
+                }
+
+                if (flag) {
+                    answer++;
+                }
+                /*
                 if (arr[i - 1][j] < arr[i][j] && arr[i][j] > arr[i + 1][j]
                 && arr[i][j - 1] < arr[i][j] && arr[i][j] > arr[i][j + 1]) {
                     answer++;
                 }
+                */
             }
-            //System.out.println(" ");
         }
         
         return answer;
@@ -33,10 +52,10 @@ public class Session10 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[][] arr = new int[n + 2][n + 2];
+        int[][] arr = new int[n][n];
 
-        for (int i = 1; i < n + 1; i++) {
-            for(int j = 1; j < n + 1; j++) {
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 arr[i][j] = kb.nextInt();
             };
         }
