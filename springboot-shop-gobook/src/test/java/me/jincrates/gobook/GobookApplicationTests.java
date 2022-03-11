f3c283cfacdfa907dc40903df37410da9fe959e8
jincrates <94jingyu@gmail.com>
2022-03-12 00:03:18 +0900

    코드 리팩터링

--- springboot-shop-gobook/src/test/java/me/jincrates/gobook/GobookApplicationTests.java
+++ springboot-shop-gobook/src/test/java/me/jincrates/gobook/GobookApplicationTests.java
@@ -1,11 +1,13 @@
 package me.jincrates.gobook;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GobookApplicationTests {
 
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
