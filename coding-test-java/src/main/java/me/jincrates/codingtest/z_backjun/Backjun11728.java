c49a10f465edb934a00f96a8c9cbeb8c60c537fd
jincrates <94jingyu@gmail.com>
2022-03-13 21:55:42 +0900

    백준 코테

---
+++ coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun11728.java
@@ -0,0 +1,39 @@
+package me.jincrates.codingtest.z_backjun;
+
+import java.io.*;
+import java.util.*;
+
+//11728. 배열합치기
+//https://www.acmicpc.net/problem/@@@@
+public class Backjun11728 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String [] line1 = br.readLine().split(" ");
+        int size1 = Integer.parseInt(line1[0]);
+        int size2 = Integer.parseInt(line1[1]);
+        int[] result = new int[size1 + size2];
+
+        //다음 줄
+        String[] line2 = br.readLine().split(" ");
+        String[] line3 = br.readLine().split(" ");
+
+        int idx = 0;
+        for (int i = 0; i < size1; i++) {
+            result[idx++] = Integer.parseInt(line2[i]);
+        }
+        for (int i = 0; i < size2; i++) {
+            result[idx++] = Integer.parseInt(line3[i]);
+        }
+
+        Arrays.sort(result); // 오름차순 정렬
+        for(int r : result) {
+            bw.write(String.valueOf(r) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun2562.java
@@ -0,0 +1,34 @@
+package me.jincrates.codingtest.z_backjun;
+
+import java.util.Scanner;
+
+//2562. 최댓값
+//https://www.acmicpc.net/problem/@@@@
+public class Backjun2562 {
+
+    public static void solution(int[] arr) {
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                answer = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(answer);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();            
+        }
+        
+        solution(arr);
+    }
+}
