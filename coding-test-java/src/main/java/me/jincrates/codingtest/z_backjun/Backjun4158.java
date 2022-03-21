5c783fba36f05a38ec91b0cae02fc152fbbfee61
jincrates <94jingyu@gmail.com>
2022-03-21 22:37:55 +0900

    4158. CD(투포인터)

--- coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun4158.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun4158.java
@@ -3,30 +3,12 @@ package me.jincrates.codingtest.z_backjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 //4158. CD(투포인터)
 //https://www.acmicpc.net/problem/@@@@
 public class Backjun4158 {
 
-    public static int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
-
-        int p1 = 0, p2 = 0;
-
-        while (p1 < arr1.length && p2 < arr2.length) {
-            if (arr1[p1] == arr2[p2]) {
-                answer++;
-                p1++;
-                p2++;
-            } else if (arr1[p1] > arr2[p2]) {
-                p2++;
-            } else {
-                p1++;
-            }
-        }
-
-        return answer;
-    }
 /*
 4 3
 1
@@ -41,23 +23,45 @@ public class Backjun4158 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] line1 = br.readLine().split(" ");
-        int n = Integer.parseInt(line1[0]);
-        int m = Integer.parseInt(line1[1]);
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[m];
-        for (int i = 0, max = n + m; i < max; i++) {
-            int cd = Integer.parseInt(br.readLine());
+        while (true) {
+            String[] line1 = br.readLine().split(" ");
+            int n = Integer.parseInt(line1[0]);
+            int m = Integer.parseInt(line1[1]);
 
-            if (cd > 0) {
-                if (i < n) {
-                    arr1[i] = cd;
+            if (n ==0 && m == 0) {
+                break;
+            }
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[m];
+
+            for (int i = 0; i < n; i++) {
+                arr1[i] = Integer.parseInt(br.readLine());
+            }
+
+            for (int i = 0; i < m; i++) {
+                arr2[i] = Integer.parseInt(br.readLine());
+            }
+
+            int answer = 0;
+            int p1 = 0, p2 = 0;
+
+            while (true) {
+                if (p1 == arr1.length || p2 == arr2.length) {
+                    break;
+                }
+                if (arr1[p1] == arr2[p2]) {
+                    answer++;
+                    p1++;
+                    p2++;
+                } else if (arr1[p1] > arr2[p2]) {
+                    p2++;
                 } else {
-                    arr2[i - n] = cd;
+                    p1++;
                 }
             }
-        }
 
-        System.out.println(solution(arr1, arr2));
+            System.out.println(answer);
+        }
     }
 }
