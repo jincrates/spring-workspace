9307b8ccec17cc703b08c92ab8b6bf86621e5c2d
jincrates <94jingyu@gmail.com>
2022-03-21 22:51:46 +0900

    10025. 게으른 백곰

--- coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun10025.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/z_backjun/Backjun10025.java
@@ -10,13 +10,21 @@ import java.util.Scanner;
 //https://www.acmicpc.net/problem/@@@@
 public class Backjun10025 {
 
-    public static int solution(int n, int k, int[] arr) {
-        int answer = 0, lt = 0, cnt = 0, sum = 0;
+    public static int solution(int k, int[] arr) {
+        int answer = 0, lt = 0, sum = 0;
+        int move = k * 2 + 1; //좌우 모두 이동 가능하기 때문
         //xi = 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ....
         //gi = _ 5 2 _ _ _ _ 4 _ _ __ __ __ __ __ 10 ...
 
-        for (int rt = 0; rt < 16; rt++) {
-            System.out.print(arr[rt] + " ");
+        for (int rt = 0; rt < arr.length; rt++) {
+            sum += arr[rt];
+            answer = Math.max(answer, sum);
+
+            //창문이동
+            if (rt >= move) {
+                sum -= arr[lt];
+                lt++;
+            }
         }
 
         return answer;
@@ -36,14 +44,15 @@ public class Backjun10025 {
         int k = Integer.parseInt(line1[1]);
         int[] arr = new int[1000001];
         int g = 0, x = 0;
+        int max = 0;
 
         for (int i = 0; i < n; i++) {
             String[] line2 = br.readLine().split(" ");
             g = Integer.parseInt(line2[0]);
             x = Integer.parseInt(line2[1]);
-            arr[x] = g;
+            arr[x] = g;  // 해당 좌표에 얼음 배치
         }
 
-        System.out.println(solution(n, k, arr));
+        System.out.println(solution(k, arr));
     }
 }
