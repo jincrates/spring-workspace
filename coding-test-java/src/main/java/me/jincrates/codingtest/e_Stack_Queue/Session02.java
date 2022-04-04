70f4f48e5321616277c7031d5d7f63f29eaed49a
jincrates <94jingyu@gmail.com>
2022-04-05 07:58:01 +0900

    2. 괄호 문자 제거

---
+++ coding-test-java/src/main/java/me/jincrates/codingtest/e_Stack_Queue/Session02.java
@@ -0,0 +1,45 @@
+package me.jincrates.codingtest.e_Stack_Queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//2. 괄호 문자 제거
+public class Session02 {
+    public static String solution(String str) {
+        String answer = "";
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char x : str.toCharArray()) {
+            //1. stack에 푸쉬
+            stack.push(x);
+
+            //2. 닫는 괄호를 만났을때, 최근 여는 괄호까지 pop
+            if (x == ')') {
+                for (int i = stack.lastIndexOf('('), max = stack.size(); i < max; i++) {
+                    stack.pop();
+                }
+            }
+        }
+
+        for (int i = 0, max = stack.size(); i < max; i++) {
+            answer += stack.elementAt(i);
+        }
+
+        return answer;
+    }
+
+/*
+(A(BC)D)EF(G(H)(IJ)K)LM(N)
+
+*/
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+}
