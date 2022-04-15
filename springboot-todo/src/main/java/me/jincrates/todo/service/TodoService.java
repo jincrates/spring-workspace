2db8d320d6142ed96183ea1d3ba52c332ea111c3
jincrates <94jingyu@gmail.com>
2022-04-15 22:55:50 +0900

    delete Todo List

--- springboot-todo/src/main/java/me/jincrates/todo/service/TodoService.java
+++ springboot-todo/src/main/java/me/jincrates/todo/service/TodoService.java
@@ -64,4 +64,24 @@ public class TodoService {
         // 5. retrieve 메서드를 이용해 사용자의 모든 Todo 리스트를 리턴한다.
         return retrieve(entity.getUserId());
     }
+
+    public List<Todo> delete(final Todo entity) {
+        //1. 저장할 엔티티가 유효한지 확인한다.
+        validate(entity);
+
+        try {
+            //2. 엔티티를 삭제한다.
+            repository.delete(entity);
+        } catch (Exception e) {
+            //3. exception 발생시 id와 exception을 로깅한다.
+            log.warning("error deleting entity " + entity.getId());
+            log.warning("error : " + e);
+
+            //4. 컨트롤러로 exception을 보낸다. 데이터베이스 내부 로직을 캡슐화하려면 e를 리턴하지 않고 새 exception 오브젝트를 리턴한다.
+            throw new RuntimeException("error deleting entity " + entity.getId());
+        }
+
+        //5. 새 Todo 리스트를 가져와 리턴한다.
+        return retrieve(entity.getUserId());
+    }
 }
