0b751f91ae87115240127887e84b1906879d9ebd
jincrates <94jingyu@gmail.com>
2022-04-21 07:46:00 +0900

    DTO 설계

--- springboot-hr/src/main/java/me/jincrates/hr/web/dto/annual/AnnualDTO.java
+++ springboot-hr/src/main/java/me/jincrates/hr/web/dto/annual/AnnualDTO.java
@@ -1,4 +1,48 @@
 package me.jincrates.hr.web.dto.annual;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.jincrates.hr.domain.annual.Annual;
+import me.jincrates.hr.domain.employees.Employee;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AnnualDTO {
+
+    private Employee employee;
+
+    //사원 입사일
+    private String empJoinDate;
+
+    //근속연수
+    private double lengthOfService;
+
+    //발생월차
+    private double hadMonthly;
+
+    //발생연차
+    private double hadAnnual;
+
+    //조정연차
+    private double adjustedAnnual;
+
+    //사용연차
+    private double usedAnnual;
+
+    public AnnualDTO(Annual entity) {
+        this.employee = entity.getEmployee();
+        this.empJoinDate = entity.getEmpJoinDate();
+        this.lengthOfService = entity.getLengthOfService();
+        this.hadMonthly = entity.getHadMonthly();
+        this.hadAnnual = entity.getHadAnnual();
+        this.adjustedAnnual = entity.getAdjustedAnnual();
+        this.usedAnnual = entity.getUsedAnnual();
+    }
 }
--- springboot-hr/src/main/java/me/jincrates/hr/web/dto/attendance/AttendanceDTO.java
+++ springboot-hr/src/main/java/me/jincrates/hr/web/dto/attendance/AttendanceDTO.java
@@ -1,4 +1,47 @@
 package me.jincrates.hr.web.dto.attendance;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.jincrates.hr.domain.attendance.Attendance;
+import me.jincrates.hr.domain.attendance.AttendanceStatus;
+import me.jincrates.hr.domain.employees.Employee;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AttendanceDTO {
+
+    private Employee employee;
+
+    private String workDate;
+
+    private LocalDateTime inDate;
+
+    private LocalDateTime outDate;
+
+    private int breakTime;
+
+    private int overTime;
+
+    private boolean isLate;
+
+    private boolean isHome;
+
+    private AttendanceStatus status;
+
+    public AttendanceDTO(Attendance entity) {
+        this.employee = entity.getEmployee();
+        this.workDate = entity.getWorkDate();
+        this.inDate = entity.getInDate();
+        this.outDate = entity.getOutDate();
+        this.breakTime = entity.getBreakTime();
+        this.overTime = entity.getOverTime();
+        this.isLate = entity.isLate();
+        this.isHome = entity.isHome();
+    }
 }
