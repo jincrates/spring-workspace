f4da2478d56deab6f2cd9068dc9551fbf8a19249
jincrates <94jingyu@gmail.com>
2022-04-21 23:10:11 +0900

    사용자 등록 api

--- springboot-todo/src/main/java/me/jincrates/todo/web/UserController.java
+++ springboot-todo/src/main/java/me/jincrates/todo/web/UserController.java
@@ -2,7 +2,9 @@ package me.jincrates.todo.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import me.jincrates.todo.domain.users.User;
 import me.jincrates.todo.service.UserService;
+import me.jincrates.todo.web.dto.ResponseDTO;
 import me.jincrates.todo.web.dto.UserDTO;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +24,27 @@ public class UserController {
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
         try {
             //요청을 이용해 저장할 사용자 만들기
-        } catch(Exception e) {
+            User user = User.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+            //서비스를 이용해 리포지터리에 사용자 저장
+            User registeredUser = userService.create(user);
 
+            //사용자 정보는 항상 하나이므로 리스트로 만들어야 하는 ResponseDTO를 사용하지 않고 그냥 UserDTO 리턴
+            UserDTO response = UserDTO.builder()
+                    .email(registeredUser.getEmail())
+                    .username(registeredUser.getUsername())
+                    .id(registeredUser.getId())
+                    .build();
+
+            return ResponseEntity.ok().body(response);
+        } catch(Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error(e.getMessage())
+                    .build();
+            return ResponseEntity.badRequest().body(responseDTO);
         }
-        return null;
     }
 }
