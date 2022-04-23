3de5df192d1b3dcdd1ebc49d48fd65f47276c9c3
jincrates <94jingyu@gmail.com>
2022-04-23 09:57:42 +0900

    출근 기능 설계중

--- springboot-hr/src/main/java/me/jincrates/hr/domain/attendance/AttendanceRepository.java
+++ springboot-hr/src/main/java/me/jincrates/hr/domain/attendance/AttendanceRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    List<Attendance> findByEmail(String empEmail);
 
 }
