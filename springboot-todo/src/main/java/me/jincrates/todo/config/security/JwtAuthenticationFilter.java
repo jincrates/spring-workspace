7177234edc6db948af7ced1af864b05eabc0737d
jincrates <94jingyu@gmail.com>
2022-04-23 20:09:42 +0900

    CorsFilter.class Import 경로를 잘못 지정하여 오류가 있었다.

--- springboot-todo/src/main/java/me/jincrates/todo/config/security/JwtAuthenticationFilter.java
+++ springboot-todo/src/main/java/me/jincrates/todo/config/security/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package me.jincrates.todo.config.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -28,28 +29,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-            //요청에서 토큰 가져오기
+            // 리퀘스트에서 토큰 가져오기.
             String token = parseBearerToken(request);
             log.info("Filter is running...");
-            //토큰 검사하기. JWT이므로 인가 서버에 요청하지 않고도 검증 가능
-            if (token != null && token.equalsIgnoreCase("null")) {
-                //userId 가져오기. 위조된 경우 예외 처리된다.
+            // 토큰 검사하기. JWT이므로 인가 서버에 요청 하지 않고도 검증 가능.
+            if (token != null && !token.equalsIgnoreCase("null")) {
+                // userId 가져오기. 위조 된 경우 예외 처리 된다.
                 String userId = tokenProvider.validateAndGetUserId(token);
-                log.info("Authenticated user ID : " + userId);
-
-                //인증 완료. SecurityContextHolder에 등록해야 인증된 사용자라고 생각한다.
+                log.info("Authenticated user ID : " + userId );
+                // 인증 완료; SecurityContextHolder에 등록해야 인증된 사용자라고 생각한다.
                 AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userId,  //인증된 사용자의 정보. 문자열이 아니어도 아무것이나 넣을 수 있다. 보통 UserDetails라는 오브젝트를 넣기도 한다.
-                        null,
+                        userId, // 인증된 사용자의 정보. 문자열이 아니어도 아무거나 넣을 수 있다.
+                        null, //
                         AuthorityUtils.NO_AUTHORITIES
                 );
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
                 securityContext.setAuthentication(authentication);
-
                 SecurityContextHolder.setContext(securityContext);
             }
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             logger.error("Could not set user authentication in security context", ex);
         }
 
@@ -57,13 +56,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String parseBearerToken(HttpServletRequest request) {
-        //Http 요청의 헤더를 파싱해 Bearer 토큰을 리턴한다.
+        // Http 리퀘스트의 헤더를 파싱해 Bearer 토큰을 리턴한다.
         String bearerToken = request.getHeader("Authorization");
 
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
-
         return null;
     }
 }
