a4342aa2eca09e48cd85f6d10c40dd85b8580630
jincrates <94jingyu@gmail.com>
2022-05-05 21:30:55 +0900

    spring webflux project init

---
+++ springboot-reactive/src/main/java/me/jincrates/reactive/controller/ServerController.java
@@ -0,0 +1,4 @@
+package me.jincrates.reactive.controller;
+
+public class ServerController {
+}
---
+++ springboot-reactive/src/test/java/me/jincrates/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
