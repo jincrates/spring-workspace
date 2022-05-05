6dc7382470c23e6ae0632509f6bbccc60c7d4d15
jincrates <94jingyu@gmail.com>
2022-05-05 21:55:55 +0900

    스프링부트 웹 애플리케이션 만들기

---
+++ springboot-reactive/src/main/java/me/jincrates/reactive/web/controller/ServerController.java
@@ -0,0 +1,23 @@
+package me.jincrates.reactive.web.controller;
+
+import me.jincrates.reactive.domain.Dish;
+import me.jincrates.reactive.service.KitchenService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ServerController {
+
+    private final KitchenService kitchen;
+
+    public ServerController(KitchenService kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> serveDishes() {
+        return this.kitchen.getDishes();
+    }
+}
