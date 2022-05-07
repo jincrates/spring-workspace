d9b54a6c781b0af3295ea0733103391db10da80a
jincrates <94jingyu@gmail.com>
2022-05-07 16:33:45 +0900

    연차 계산식 추가(결합도 낮춤)

--- springboot-hr/src/main/java/me/jincrates/hr/domain/annual/Annual.java
+++ springboot-hr/src/main/java/me/jincrates/hr/domain/annual/Annual.java
@@ -1,10 +1,13 @@
 package me.jincrates.hr.domain.annual;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.jincrates.hr.domain.employees.Employee;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @NoArgsConstructor
@@ -39,14 +42,55 @@ public class Annual {
     //사용연차
     private double usedAnnual;
 
+    private double calculateMonthly(String annualType, String joinDate, String baseDate) {
+        double result = 0;
 
+        //근속연월일
+        double monthsBetween = betweenMonths(joinDate, baseDate);
+        double daysBetween = betweenDays(joinDate, baseDate);
 
-    //회계연도 기준
-    //근속연수= 현재연도 - 입사연도
-    //1. 회계연도 생성 기준일로부터 1년 이상 근무자인지?
-    //1-a. 이상이다. 근속연수에 비례하여 연차 부여: 15 + (근속연수/2)
-    //1-b. 미만이다. 근무한 월만큼 연차부여(최대 11개)
-    //1-c. 비례연차 대상자의 경우
+        //1년 미만 입사자: 발생월차(개정연차) 계산
+        if (daysBetween < 365.0) {
+            result = monthsBetween;
+        }
 
+        //음수 체크
+        return (result < 0) ? 0 : result;
+    }
 
+    public double calculateAnnual(String annualType, String joinDate, String baseDate) {
+        double result = 0;
+
+        //근속연월일
+        double yearsBetween = betweenYears(joinDate, baseDate);
+        double monthsBetween = betweenMonths(joinDate, baseDate);
+        double daysBetween = betweenDays(joinDate, baseDate);
+
+        //1년 미만 연차계산
+        if (daysBetween < 365.0) {
+            //회계연도 기준일때 비례연차 계산 추가(비례연차 대상자: 기준일 이전 연도 입사자)
+            //비례연차 계산: 전연도 재직일수 / 365 * 15
+            if (annualType.equals("F") && yearsBetween > 0) {
+                result += Math.floor(daysBetween / 365.0 * 15);
+            }
+            //1년 이상 연차계산: 최초발생 연차 + ((근속연수 - 1년) / 2) 계산 후 나머지는 버림
+        } else {
+            result = 15 + Math.floor((monthsBetween / 12.0 - 1) / 2.0);
+        }
+
+        //음수 체크
+        return (result < 0) ? 0 : result;
+    }
+
+    private double betweenYears(String fromDate, String toDate) {
+        return Math.abs(LocalDate.parse(fromDate).getYear() - LocalDate.parse(toDate).getYear());
+    }
+
+    private double betweenMonths(String fromDate, String toDate) {
+        return ChronoUnit.MONTHS.between(LocalDate.parse(fromDate), LocalDate.parse(toDate));
+    }
+
+    private double betweenDays(String fromDate, String toDate) {
+        return ChronoUnit.DAYS.between(LocalDate.parse(fromDate), LocalDate.parse(toDate));
+    }
 }
