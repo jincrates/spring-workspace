43046292c0e59a90fc78bf28d04c2cb22250d73b
jincrates <94jingyu@gmail.com>
2022-05-12 08:43:23 +0900

    Example 쿼리로 구현된 서비스를 사용하는 웹 컨트롤러 구현중

--- springboot-reactive-book/src/main/java/me/jincrates/reactivebook/web/HomeController.java
+++ springboot-reactive-book/src/main/java/me/jincrates/reactivebook/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -33,5 +34,14 @@ public class HomeController {
     Mono<String> addToCart(@PathVariable String id) {
         return this.cartService.addToCart("My Cart", id)
                 .thenReturn("redirect:/");
+    }
+
+    @GetMapping("/search")
+    Mono<Rendering> search(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description,
+            @RequestParam boolean useAnd) {
+        return null;
+
     }
 }
