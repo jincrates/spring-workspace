bf179f2c0831e4a49fae4cf64f50785e31568506
jincrates <94jingyu@gmail.com>
2022-05-17 08:42:30 +0900

    사원등록 구현중

--- springboot-hr/src/main/java/me/jincrates/hr/web/EmployeeController.java
+++ springboot-hr/src/main/java/me/jincrates/hr/web/EmployeeController.java
@@ -1,14 +1,24 @@
 package me.jincrates.hr.web;
 
 import lombok.RequiredArgsConstructor;
+import me.jincrates.hr.domain.employees.Employee;
+import me.jincrates.hr.service.employees.EmployeeService;
 import me.jincrates.hr.web.dto.employees.EmployeeDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/login")
     public String loginEmployee() {
@@ -30,5 +40,25 @@ public class EmployeeController {
     public String employeeForm(Model model) {
         model.addAttribute("employeeFormDTO", new EmployeeDTO());
         return "employee/employeeForm";
+    }
+
+    @PostMapping(value = "/employee/save")
+    public String employeeForm(@Valid EmployeeDTO employeeDTO, BindingResult bindingResult, Model model) {
+        System.out.println(employeeDTO.toString());
+        
+        if (bindingResult.hasErrors()) {
+            return "employee/employeeForm";
+        }
+
+        try {
+            Employee employee = Employee.createEmployee(employeeDTO, passwordEncoder);
+            employeeService.create(employee);
+
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "employee/employeeForm";
+        }
+
+        return "redirect:/";
     }
 }
