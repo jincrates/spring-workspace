81af2a9f4582a51ea3824ffcf323e5eed42da062
jincrates <94jingyu@gmail.com>
2022-06-07 08:18:36 +0900

    5. 동전교환(개선)

--- coding-test-java/src/main/java/me/jincrates/codingtest/h_DFS_BFS/Session05.java
+++ coding-test-java/src/main/java/me/jincrates/codingtest/h_DFS_BFS/Session05.java
@@ -1,17 +1,24 @@
 package me.jincrates.codingtest.h_DFS_BFS;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 //5. 동전교환
 public class Session05 {
     static int n, m, answer = Integer.MAX_VALUE;
 
-    public void DFS(int level, int sum, int[] arr) {
+    public void DFS(int level, int sum, Integer[] arr) {
         //무한재귀에 빠지지 않도록 처리
         if (sum > m) {
             return;
         }
+        //시간복잡도를 줄이기 위해
+        if (level >= answer) {
+            return;
+        }
+
         if (sum == m) {
             //level: sum을 만드는 동전의 개수
             answer = Math.min(answer, level);
@@ -28,12 +35,17 @@ public class Session05 {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        int[] arr = new int[n];
+        Integer[] arr = new Integer[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        //큰 수를 먼저처리하도록 내림차순 정렬
+        //해당 함수를 사용하기 위해서는 배열의 타입을 객체형으로 바꿔줘야한다.
+        Arrays.sort(arr, Collections.reverseOrder());
 
         m = sc.nextInt();
+
+
         main.DFS(0, 0, arr);
         System.out.println(answer);
     }
