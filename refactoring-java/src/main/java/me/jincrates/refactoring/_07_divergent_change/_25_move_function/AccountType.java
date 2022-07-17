935574e6dec9d5f11bd0832bfddd8aabdb20912d
jincrates <94jingyu@gmail.com>
2022-07-17 13:23:26 +0900

    리팩토링 25. 함수 옮기기(Move Function)

--- refactoring-java/src/main/java/me/jincrates/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ refactoring-java/src/main/java/me/jincrates/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -10,4 +10,18 @@ public class AccountType {
     public boolean isPremium() {
         return this.premium;
     }
+
+    //Account에 있어도 적절할 수 있지만, AccountType의 type을 사용하고 있기에 옮겨보자.
+    double overdraftCharge(int daysOverdrawn) {
+        if (this.isPremium()) {
+            final int baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
