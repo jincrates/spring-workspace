0b3f599a88a7a3803156dd8636caea54eb286f86
jincrates <94jingyu@gmail.com>
2022-07-17 14:52:01 +0900

    LessThan, OrderBy 테스트 코드 작성

--- springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
+++ springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
@@ -7,4 +7,11 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(String title);
+
+    List<Book> findByTitleOrAuthor(String title, String author);
+
+    List<Book> findByStockNumberLessThan(Integer stock);
+
+    List<Book> findByStockNumberLessThanOrderByStockNumberDesc(Integer stock);
 }
+
--- springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
+++ springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
@@ -18,18 +18,6 @@ class BookRepositoryTest {
     @Autowired
     BookRepository bookRepository;
 
-    public void createBookList() {
-        IntStream.rangeClosed(1, 10).mapToObj(i -> Book.builder()
-                .title("철학적 탐구" + i)
-                .author("루트비히 비트겐슈타인")
-                .publisher("책세상")
-                .publicationDate("2019-04-05")
-                .isbn("9791159313554")
-                .stockNumber(10 + i)
-                .imagePath("")
-                .build()).forEach(book -> bookRepository.save(book));
-    }
-
     @Test
     @DisplayName("도서 저장 테스트")
     public void createBookTest() {
@@ -57,7 +45,7 @@ class BookRepositoryTest {
 
     @Test
     @DisplayName("도서명 조회 테스트")
-    public void findByTitle() {
+    public void findByTitleTest() {
         this.createBookList();
         List<Book> bookList = bookRepository.findByTitle("철학적 탐구2");
 
@@ -69,5 +57,58 @@ class BookRepositoryTest {
 
         assertNotNull(bookList);
         assertEquals("철학적 탐구2", resultTitle);
+    }
+
+    @Test
+    @DisplayName("도서명 or 저자 조회 테스트")
+    public void findByTitleOrAuthorTest() {
+        this.createBookList();
+        List<Book> bookList = bookRepository.findByTitleOrAuthor("철학적 탐구2", "루트비히 비트겐슈타인5");
+
+        print(bookList);
+
+        assertNotNull(bookList);
+        assertEquals(2, bookList.size());
+    }
+
+    @Test
+    @DisplayName("재고 LessThan 테스트")
+    public void findByStockNumberLessThanTest() {
+        this.createBookList();
+        List<Book> bookList = bookRepository.findByStockNumberLessThan(15);
+        print(bookList);
+
+        assertNotNull(bookList);
+        assertEquals(4, bookList.size());
+    }
+
+    @Test
+    @DisplayName("재고 LessThan + 내림차순 조회 테스트")
+    public void findByStockNumberLessThanOrderByStockNumberDescTest() {
+        this.createBookList();
+        List<Book> bookList = bookRepository.findByStockNumberLessThanOrderByStockNumberDesc(15);
+        print(bookList);
+
+        assertNotNull(bookList);
+        assertEquals(4, bookList.size());
+        assertEquals(4, bookList.get(0).getId());
+    }
+
+    public void createBookList() {
+        IntStream.rangeClosed(1, 10).mapToObj(i -> Book.builder()
+                .title("철학적 탐구" + i)
+                .author("루트비히 비트겐슈타인" + i)
+                .publisher("책세상" + i)
+                .publicationDate("2019-04-05")
+                .isbn("9791159313554")
+                .stockNumber(10 + i)
+                .imagePath("")
+                .build()).forEach(book -> bookRepository.save(book));
+    }
+
+    private void print(List<Book> bookList) {
+        for (Book book : bookList) {
+            System.out.println(book.toString());
+        }
     }
 }
