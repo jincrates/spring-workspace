1a26f99c2deff6f231af20f9d9b609d8d3aaa75c
jincrates <94jingyu@gmail.com>
2022-07-17 15:25:23 +0900

    QueryDsl 추가 테스트 코드 작성

--- springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
+++ springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
@@ -1,17 +1,26 @@
 package me.jincrates.bookmanager.domain.books;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>,
+        QuerydslPredicateExecutor<Book> {
 
     List<Book> findByTitle(String title);
 
     List<Book> findByTitleOrAuthor(String title, String author);
 
+    List<Book> findByTitleOrAuthorOrPublisher(String title, String author, String publisher);
+
     List<Book> findByStockNumberLessThan(Integer stock);
 
     List<Book> findByStockNumberLessThanOrderByStockNumberDesc(Integer stock);
+
+    @Query("select b from Book b where b.publisher like %:publisher% order by b.id desc")
+    List<Book> findByPublisher(@Param("publisher") String publisher);
 }
 
--- springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
+++ springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
@@ -1,11 +1,19 @@
 package me.jincrates.bookmanager.domain.books;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -17,6 +25,9 @@ class BookRepositoryTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     @DisplayName("도서 저장 테스트")
@@ -94,14 +105,98 @@ class BookRepositoryTest {
         assertEquals(4, bookList.get(0).getId());
     }
 
+    @Test
+    @DisplayName("@Query를 이용한 출판사 조회 테스트")
+    public void findByPublisherTest() {
+        this.createBookList();
+        List<Book> bookList = bookRepository.findByPublisher("책세상");
+        print(bookList);
+
+        assertNotNull(bookList);
+        assertEquals(10, bookList.size());
+    }
+
+    @Test
+    @DisplayName("QueryDsl 조회 테스트1")
+    public void queryDslTest1() {
+        this.createBookList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QBook qBook = QBook.book;
+        JPAQuery<Book> query = queryFactory.selectFrom(qBook)
+                .where(qBook.isbn.eq("9791159313554"))
+                .where(qBook.title.like("%" + "철학적 탐구" + "%"))
+                .orderBy(qBook.stockNumber.desc());
+        List<Book> bookList = query.fetch();
+        print(bookList);
+
+        assertNotNull(bookList);
+        assertEquals(10, bookList.size());
+    }
+
+    @Test
+    @DisplayName("QueryDsl 조회 테스트2")
+    public void queryDslTest2() {
+        this.createBookList2();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QBook qbook = QBook.book;
+
+        String title = "말과 사물";
+        String author = "미셸 푸코";
+        int stockNumber = 20;
+
+        booleanBuilder.and(qbook.title.like("%" + title + "%"));
+        booleanBuilder.and(qbook.author.like("%" + author + "%"));
+        booleanBuilder.and(qbook.stockNumber.gt(stockNumber));
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Book> bookPagingResult = bookRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + bookPagingResult.getTotalElements());
+
+        List<Book> resultBookList = bookPagingResult.getContent();
+        print(resultBookList);
+    }
+
     public void createBookList() {
-        IntStream.rangeClosed(1, 10).mapToObj(i -> Book.builder()
+        IntStream.rangeClosed(1, 5).mapToObj(i -> Book.builder()
                 .title("철학적 탐구" + i)
                 .author("루트비히 비트겐슈타인" + i)
                 .publisher("책세상" + i)
                 .publicationDate("2019-04-05")
                 .isbn("9791159313554")
                 .stockNumber(10 + i)
+                .imagePath("")
+                .build()).forEach(book -> bookRepository.save(book));
+    }
+
+    public void createBookList2() {
+        IntStream.rangeClosed(6, 10).mapToObj(i -> Book.builder()
+                .title("철학적 탐구" + i)
+                .author("루트비히 비트겐슈타인" + i)
+                .publisher("책세상" + i)
+                .publicationDate("2019-04-05")
+                .isbn("9791159313554")
+                .stockNumber(10 + i)
+                .imagePath("")
+                .build()).forEach(book -> bookRepository.save(book));
+
+        IntStream.rangeClosed(11, 15).mapToObj(i -> Book.builder()
+                .title("말과 사물" + i)
+                .author("미셸 푸코" + i)
+                .publisher("민음사" + i)
+                .publicationDate("2012-04-29")
+                .isbn("9788937484414")
+                .stockNumber(20 + i)
+                .imagePath("")
+                .build()).forEach(book -> bookRepository.save(book));
+
+        IntStream.rangeClosed(21, 30).mapToObj(i -> Book.builder()
+                .title("방법서설" + i)
+                .author("르네 데카르트" + i)
+                .publisher("문예출판사" + i)
+                .publicationDate("2022-05-30")
+                .isbn("9788931022759")
+                .stockNumber(30 + i)
                 .imagePath("")
                 .build()).forEach(book -> bookRepository.save(book));
     }
