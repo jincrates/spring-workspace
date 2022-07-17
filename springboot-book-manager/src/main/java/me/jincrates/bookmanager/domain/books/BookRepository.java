d21b72bd1e4f0c117d07630b33629758ad3c1d7c
jincrates <94jingyu@gmail.com>
2022-07-17 14:32:16 +0900

    도서명 조회 테스트 코드 작성

--- springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
+++ springboot-book-manager/src/main/java/me/jincrates/bookmanager/domain/books/BookRepository.java
@@ -2,5 +2,9 @@ package me.jincrates.bookmanager.domain.books;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
 }
--- springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
+++ springboot-book-manager/src/test/java/me/jincrates/bookmanager/domain/books/BookRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,6 +17,18 @@ class BookRepositoryTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    public void createBookList() {
+        IntStream.rangeClosed(1, 10).mapToObj(i -> Book.builder()
+                .title("철학적 탐구" + i)
+                .author("루트비히 비트겐슈타인")
+                .publisher("책세상")
+                .publicationDate("2019-04-05")
+                .isbn("9791159313554")
+                .stockNumber(10 + i)
+                .imagePath("")
+                .build()).forEach(book -> bookRepository.save(book));
+    }
 
     @Test
     @DisplayName("도서 저장 테스트")
@@ -29,5 +44,30 @@ class BookRepositoryTest {
                 .build();
         Book savedBook = bookRepository.save(book);
         System.out.println(savedBook);
+
+        assertNotNull(savedBook);
+        assertEquals(book.getTitle(), savedBook.getTitle());
+        assertEquals(book.getAuthor(), savedBook.getAuthor());
+        assertEquals(book.getPublisher(), savedBook.getPublisher());
+        assertEquals(book.getPublicationDate(), savedBook.getPublicationDate());
+        assertEquals(book.getIsbn(), savedBook.getIsbn());
+        assertEquals(book.getStockNumber(), savedBook.getStockNumber());
+        assertEquals(book.getImagePath(), savedBook.getImagePath());
+    }
+
+    @Test
+    @DisplayName("도서명 조회 테스트")
+    public void findByTitle() {
+        this.createBookList();
+        List<Book> bookList = bookRepository.findByTitle("철학적 탐구2");
+
+        String resultTitle = "";
+        for (Book book : bookList) {
+            //System.out.println(book.toString());
+            resultTitle = book.getTitle();
+        }
+
+        assertNotNull(bookList);
+        assertEquals("철학적 탐구2", resultTitle);
     }
 }
