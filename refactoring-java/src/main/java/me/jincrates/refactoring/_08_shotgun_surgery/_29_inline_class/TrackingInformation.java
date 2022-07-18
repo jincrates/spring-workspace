aa97edb46b6c238bfd8bded0e664366a9d8d0bad
jincrates <94jingyu@gmail.com>
2022-07-19 07:46:11 +0900

    리팩토링 29. 클래스 인라인(Inline Class)

--- refactoring-java/src/main/java/me/jincrates/refactoring/_08_shotgun_surgery/_29_inline_class/TrackingInformation.java
+++ refactoring-java/src/main/java/me/jincrates/refactoring/_08_shotgun_surgery/_29_inline_class/TrackingInformation.java
@@ -11,23 +11,4 @@ public class TrackingInformation {
         this.trackingNumber = trackingNumber;
     }
 
-    public String display() {
-        return this.shippingCompany + ": " + this.trackingNumber;
-    }
-
-    public String getShippingCompany() {
-        return shippingCompany;
-    }
-
-    public void setShippingCompany(String shippingCompany) {
-        this.shippingCompany = shippingCompany;
-    }
-
-    public String getTrackingNumber() {
-        return trackingNumber;
-    }
-
-    public void setTrackingNumber(String trackingNumber) {
-        this.trackingNumber = trackingNumber;
-    }
 }
