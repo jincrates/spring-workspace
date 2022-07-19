351f4241a918fd48b8864627c8971b8bfd847aad
jincrates <94jingyu@gmail.com>
2022-07-20 07:38:07 +0900

    회원가입 화면 생성

--- springboot-study-manager/src/main/java/me/jincrates/studymanager/account/AccountController.java
+++ springboot-study-manager/src/main/java/me/jincrates/studymanager/account/AccountController.java
@@ -10,6 +10,8 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
+        model.addAttribute(new SignUpForm());  // 이름이 같다면 이렇게도 쓸 수 있다.
+        //model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
     }
 }
--- springboot-study-manager/src/test/java/me/jincrates/studymanager/account/AccountControllerTest.java
+++ springboot-study-manager/src/test/java/me/jincrates/studymanager/account/AccountControllerTest.java
@@ -9,8 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,11 +17,13 @@ class AccountControllerTest {
 
     @Autowired private MockMvc mockMvc;
 
+    // 매번 있는지 확인할 수 없으니 테스트 코드로 확인하기 위함
     @DisplayName("회원가입 화면 보이는지 테스트")
     @Test
     void signUpFormTest() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
     }
 }
